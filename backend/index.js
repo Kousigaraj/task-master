d0d404e29e6a15fb794d8d986a411d456f34d4ef
Kousigaraj <kousigaraj77@gmail.com>
2025-08-21 22:10:33 +0530

    Completed full Task master app - version 1.0.0

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,15 @@ import autoDeleteOldTrash from './utils/cornJobs.js';
 import cookieParser from 'cookie-parser';
 import authRouter from './routes/authRoute.js';
 import userRouter from './routes/userRoute.js';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
 connectDB();
 autoDeleteOldTrash();
 
@@ -26,6 +30,12 @@ app.use("/api/tasks", taskRoutes);
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    });
+}
 
 app.get('/', (req, res) => res.send("Api Working"));
 
